08e3fa7948591b1fa57bcc0974c29d8e9d64097a
vasiioandre <vasi_ioandre@yahoo.com>
2023-10-18 22:38:40 +0300

    Update and delete ingredients

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -50,7 +50,10 @@ class TagViewSet(mixins.DestroyModelMixin,
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
 
-class IngredientViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class IngredientViewSet(mixins.DestroyModelMixin,
+                        mixins.UpdateModelMixin,
+                        mixins.ListModelMixin,
+                        viewsets.GenericViewSet):
     serializer_class = serializers.IngredientSerializer
     queryset = Ingredient.objects.all()
     authentication_classes = [TokenAuthentication]
